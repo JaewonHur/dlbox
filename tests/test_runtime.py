2de43e4510c1e330d8f03ff5e54afea2ef5a8305
JaewonHur <hurjaewon@snu.ac.kr>
2022-06-14 21:28:20 +0900

    [test] Add PrimeError test
    Check cases that should raise PrimeError.
    
    Signed-off-by: JaewonHur <hurjaewon@snu.ac.kr>

--- tests/test_runtime.py
+++ tests/test_runtime.py
@@ -13,6 +13,7 @@ import importlib.util
 
 import prime
 from prime import PrimeClient
+from prime.exceptions import PrimeError, UserError
 
 F_OUTPUT = """
 def output(x):
@@ -21,11 +22,38 @@ def output(x):
         dill.dump(x, fd)
 """
 
+F_ERROR = """
+err = 3/0
+
+def error():
+    val = err + 3
+    return val
+"""
+
 MY_CLASS = """
 class MyClass():
     def __init__(self, x):
         self.x = x
+    def doublex(self):
+        self.x *= 2
 """
+
+def export_class(name: str, src: str):
+    module = f'/tmp/_{name}.py'
+    with open(module, 'w') as fd:
+        fd.write(src)
+
+    spec = importlib.util.spec_from_file_location(name, module)
+    module = importlib.util.module_from_spec(spec)
+
+    sys.modules[name] = module
+    globals()[name] = module
+    spec.loader.exec_module(module)
+
+class NotInDE():
+    def __init__(self, x):
+        self.x = x
+
 
 logdir = f'{os.getcwd()}/test-logs'
 os.makedirs(logdir, exist_ok=True)
@@ -45,6 +73,7 @@ def import_prime(func):
         prime.utils.kill_server()
 
     return wrapper
+
 
 @import_prime
 def test_ExportDef(_client: PrimeClient):
@@ -94,16 +123,7 @@ def test_AllocateObj(_client: PrimeClient):
 
     # First define MyClass
     name = 'MyClass'
-    module = f'/tmp/_MyClass.py'
-    with open(module, 'w') as fd:
-        fd.write(MY_CLASS)
-
-    spec = importlib.util.spec_from_file_location(name, module)
-    module = importlib.util.module_from_spec(spec)
-
-    sys.modules[name] = module
-    globals()[name] = module
-    spec.loader.exec_module(module)
+    export_class(name, MY_CLASS)
 
     x = 1
     class_in_fe = MyClass.MyClass(x)
@@ -116,3 +136,40 @@ def test_AllocateObj(_client: PrimeClient):
     new_x = _client.InvokeMethod('__main__', 'getattr', [class_in_de, attr], {})
 
     assert read(new_x) == x
+
+
+# TODO: Test FitModel
+@import_prime
+def test_PrimeError(_client: PrimeClient):
+
+    # Mistmatch in function def and name should raise PrimeError
+    name = 'not_output'
+    tpe = types.FunctionType
+    with pytest.raises(PrimeError, match="module 'not_output' has no attribute 'not_output'"):
+        output = _client.ExportDef(name, tpe, F_OUTPUT)
+
+    # Allocate object which is not defined in DE should raise PrimeError
+    not_in_de = NotInDE(1)
+    with pytest.raises(PrimeError,
+                       match="type not defined: <class 'tests.test_runtime.NotInDE'>"):
+        output = _client.AllocateObj(not_in_de)
+
+    # Invoke a method on not existing object should raise PrimeError
+    with pytest.raises(PrimeError):
+        output = _client.InvokeMethod('not_in_de', 'not_method', [], {})
+
+    # Invoke an existing method on existing object with invalid argument should
+    # raise PrimeError
+    name = 'MyClass'
+    export_class(name, MY_CLASS)
+    _client.ExportDef(name, type, MY_CLASS)
+
+    class_in_fe = MyClass.MyClass(1)
+    class_in_de = _client.AllocateObj(class_in_fe)
+
+    with pytest.raises(PrimeError, match='not_in_de'):
+        output = _client.InvokeMethod(class_in_de, 'doublex', ['not_in_de'], {})
+
+    with pytest.raises(PrimeError, match='not_in_de'):
+        output = _client.InvokeMethod(class_in_de, 'doublex', [], {'not_key': 'not_in_de'})
+
