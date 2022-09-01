c753610a0f315dfac50a239e96f52c1bea146909
JaewonHur <hurjaewon@snu.ac.kr>
2022-09-01 16:05:16 +0900

    [feat] Implement FairDataset
    FairDataset fetches the supplied data and sanitizes to test all the data
    has same tag (with only different index), and all samples are used.
    
    Signed-off-by: JaewonHur <hurjaewon@snu.ac.kr>

--- prime/data.py
+++ prime/data.py
@@ -9,11 +9,53 @@ from collections import namedtuple
 from torch import Tensor
 from torch.utils.data import DataLoader, Dataset
 
-from prime.taint import Tag
+from prime.taint import Tag, TagError
 
 DataPair = namedtuple('DataPair', ['sample', 'label'])
-Sample   = namedtuple('Sample',   ['tag', 'value'])
-Label    = namedtuple('Label',    ['tag', 'value'])
+Sample   = namedtuple('Sample',   ['tag', 'val'])
+Label    = namedtuple('Label',    ['tag', 'val'])
+
+class FairDataset(Dataset):
+    N: int = None
+
+    def __init__(self, dqueue: queue.Queue):
+        super().__init__()
+
+        self.dqueue = dqueue
+        self.h = None
+
+    @classmethod
+    def set_n(cls, N: int):
+        cls.N = N
+
+    def __len__(self) -> int:
+        return self.N
+
+    def __getitem__(self, idx: int) -> Tuple[Tensor]:
+        pair = self.dqueue.get()
+
+        s_tag, l_tag  = pair.sample.tag, pair.label.tag
+        sample, label = pair.sample.val, pair.label.val
+
+        self.sanitize(idx, s_tag, l_tag)
+
+        return (sample, label)
+
+    def sanitize(self, idx: int, s_tag: Tag, l_tag: Tag):
+        # TODO: Check label tag also
+
+        if self.h is None:
+            self.h = s_tag.h
+
+        if not s_tag.m.has_only(idx):
+            raise TagError(f'tag does not match idx\n[{idx}] tag: {tag}, idx: {idx}')
+        elif not s_tag.h == self.h:
+            raise TagError(f'tag does not match has\n[{idx}] tag: {tag}, h: {hex(h)[0:5]}')
+
 
 def build_dataloader(dqueue: queue.Queue) -> DataLoader:
-    raise NotImplementedError()
+
+    dataset = FairDataset(dqueue)
+    dataloader = DataLoader(dataset)
+
+    return dataloader
