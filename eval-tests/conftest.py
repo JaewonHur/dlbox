2b99fbd013fd36ccc2d92aa18757aaea274d39a7
Jaewon Hur <hurjaewon@snu.ac.kr>
2023-03-25 01:10:52 +0000

    Remove evaluation related things
    Signed-off-by: Jaewon Hur <hurjaewon@snu.ac.kr>

--- eval-tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-
-def pytest_addoption(parser):
-    parser.addoption('--is_vm', action='store_true', default=False)
-    parser.addoption('--baseline', action='store_true', default=False)
-    parser.addoption('--dataset', action='store', default='cifar10')
-    parser.addoption('--model', action='store', default='resnet')
-    parser.addoption('--max_epochs', action='store', default=10)
